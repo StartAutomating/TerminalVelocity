aebf3df9bfb4d03d3c9948f5e43e97ef60174e1d
James Brundage <james.brundage@start-automating.com>
2020-05-17 01:16:16 -0700

    Adding Remove-WTColorScheme

---
+++ Remove-WTColorScheme.ps1
@@ -0,0 +1,32 @@
+﻿function Remove-WTColorScheme
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+    # The name of the color scheme.
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+    [Alias('Name')]
+    [string[]]
+    $ColorScheme
+    )
+
+    process {
+        #region Get Color Scheme
+        $matchingSchemes = Get-WTColorScheme @PSBoundParameters           # call Get-WTProfile
+        $matchingSchemeNames = $matchingSchemes | 
+            Select-Object -ExpandProperty Name
+        $allSchemes = Get-WTProfile -ColorScheme *
+        $updatedSchemes = @($allSchemes | 
+            Where-Object {
+                foreach ($cs in $ColorScheme) {
+                    if ($_.Name -like $cs) { return $false } 
+                }
+                return $true
+            })
+        
+        Set-WTProfile -ColorScheme $updatedSchemes
+        #endregion Get Color Scheme
+
+    }
+
+
+}
