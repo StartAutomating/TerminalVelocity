bd086c86c9250856f5866a6a17abf2221d69d689
James Brundage <@github.com>
2022-11-07 13:43:47 -0800

    New-WTProfile:  Keeping in sync with Schema (re #8)

---
+++ New-WTProfile.ps.ps1
@@ -0,0 +1,24 @@
+﻿function New-WTProfile
+{
+    <#
+    .Synopsis
+        Creates a new Windows Terminal profile
+    .Description
+        Creates a new Windows Terminal tab profile.
+    .Link
+        Add-WTProfile
+    .Example
+        New-WTProfile -Name DOS -CommandLine cmd
+    .Example
+        New-WTProfile -Name kali-linux-pwsh -CommandLine 'wsl -d kali-linux -e pwsh'
+    .Example
+        New-WTProfile -Default
+    #>
+    [OutputType('WindowsTerminal.Profile')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSUseShouldProcessForStateChangingFunctions", "", Justification="Not changing state"
+    )]
+    [JSONSchema(SchemaUri='https://aka.ms/terminal-profiles-schema#Profile',RemovePropertyPrefix='Experimental')]
+    param()
+    
+}
